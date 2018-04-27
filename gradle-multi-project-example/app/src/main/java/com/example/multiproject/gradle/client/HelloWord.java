bd1eb62c91c363c645f54da2c7914c3a8ce85b94
Marcos Portela <marcosnasp@gmail.com>
2018-04-27 11:15:59 -0300

    Adding Log4j2 support

--- gradle-multi-project-example/app/src/main/java/com/example/multiproject/gradle/client/HelloWord.java
+++ gradle-multi-project-example/app/src/main/java/com/example/multiproject/gradle/client/HelloWord.java
@@ -1,14 +1,15 @@
 package com.example.multiproject.gradle.client;
 
 import com.example.multiproject.gradle.core.MessageService;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HelloWord {
 
-    public static final Logger LOGGER = Logger.getLogger(HelloWord.class);
+    private static final Logger LOGGER = LogManager.getLogger(HelloWord.class);
 
     public static void main(String[] args) {
         MessageService messageService = new MessageService();
-        System.out.println(messageService.getMessage());
+        LOGGER.info(messageService.getMessage());
     }
 }
